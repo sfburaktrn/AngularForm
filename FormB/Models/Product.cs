1f67aff904307d6e125ff0b1fe668f1a1c276518
sfburaktrn <sbtorun.sbt@gmail.com>
2023-08-15 16:00:35 +0300

    database start

---
+++ FormB/Models/Product.cs
@@ -0,0 +1,13 @@
+namespace efcorewithsqlite.Models
+{
+
+    public class Product 
+    {
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public int Age { get; set; }
+    public string? City { get; set; }
+    }
+}
